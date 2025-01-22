ee331d8477c48e44af5b68a9efdab623b4ca7801
Jesse <3191955858@qq.com>
2025-01-22 13:52:35 +0800

    Cmake 编译中间文件

---
+++ build/CMakeFiles/Test_ParallelAccumulate.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_ParallelAccumulate.dir/Test/Test_ParallelAccumulate.cpp.obj"
+  "CMakeFiles/Test_ParallelAccumulate.dir/Test/Test_ParallelAccumulate.cpp.obj.d"
+  "F:/Thread-Pool/bin/Test_ParallelAccumulate.exe"
+  "F:/Thread-Pool/bin/Test_ParallelAccumulate.exe.manifest"
+  "F:/Thread-Pool/bin/Test_ParallelAccumulate.pdb"
+  "libTest_ParallelAccumulate.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_ParallelAccumulate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
